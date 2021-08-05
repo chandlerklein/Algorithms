163f0e2c55f45e82f750b20bcda872d5070684f6
Chandler Klein <kleinc16@gmail.com>
2021-08-05 17:24:51 -0400

    Refactored sorting

--- src/com/chandler/algorithms/sorting/InsertionSort.java
+++ src/com/chandler/algorithms/sorting/InsertionSort.java
@@ -1,69 +1,26 @@
 package com.chandler.algorithms.sorting;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
 public class InsertionSort {
 
-    public static void sort(int n, int numTimes) {
-        System.out.printf("Insertion sorting array with size of %d %d times\n", n, numTimes);
-        var times = new ArrayList<Long>();
-
-        for (int x = 0; x < numTimes; x++) {
-            int[] arr = new Random().ints(n, 0, 101).toArray();
-            var start = System.currentTimeMillis();
-
-            for (int i = 1; i < arr.length; i++) {
-                int key = arr[i];
-                int j = i - 1;
-                while (j >= 0 && arr[j] > key) {
-                    arr[j + 1] = arr[j];
-                    j--;
-                }
-                arr[j + 1] = key;
+    public static void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            // non-increasing: arr[j] < key
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
             }
-            System.out.println(Arrays.toString(arr));
-
-            var finish = System.currentTimeMillis();
-            var timeElapsed = finish - start;
-            times.add(timeElapsed);
+            arr[j + 1] = key;
         }
-
-        long averageTime = times.stream().mapToLong(Long::valueOf).sum() / times.size();
-        System.out.printf("Insertion sort completed - average sort time of %sms", averageTime);
-    }
-
-    public static void sortDecreasing(int n, int numTimes) {
-        System.out.printf("Insertion sorting array with size of %d %d times\n", n, numTimes);
-        var times = new ArrayList<Long>();
-
-        for (int x = 0; x < numTimes; x++) {
-            int[] arr = new Random().ints(n, 0, 101).toArray();
-            var start = System.currentTimeMillis();
-
-            for (int i = 1; i < arr.length; i++) {
-                int key = arr[i];
-                int j = i - 1;
-                while (j >= 0 && arr[j] < key) {
-                    arr[j + 1] = arr[j];
-                    j--;
-                }
-                arr[j + 1] = key;
-            }
-            System.out.println(Arrays.toString(arr));
-
-            var finish = System.currentTimeMillis();
-            var timeElapsed = finish - start;
-            times.add(timeElapsed);
-        }
-
-        long averageTime = times.stream().mapToLong(Long::valueOf).sum() / times.size();
-        System.out.printf("Insertion sort decreasing completed - average sort time of %sms", averageTime);
     }
 
     public static void main(String[] args) {
-        InsertionSort.sortDecreasing(10, 100);
+        var arr = new Random().ints(30, 0, 101).toArray();
+        InsertionSort.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
-
 }
